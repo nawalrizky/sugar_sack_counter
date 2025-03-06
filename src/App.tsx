775a4c1bb9041d10ca0f1a48adb2cba9de9119f3
nawalrizky <nawalrizkykautsar@mail.ugm.ac.id>
2025-03-06 13:23:24 +0700

    revise

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import MainLayout from './layouts/MainLayout';
 import Dashboard from './components/Dashboard';
